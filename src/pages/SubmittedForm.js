30299725e790d367e843f77d66df753df2140476
Gouthami938 <70152870+Gouthami938@users.noreply.github.com>
2022-10-05 23:06:00 +0530

    project frontend

---
+++ src/pages/SubmittedForm.js
@@ -0,0 +1,34 @@
+import Base from "../components/Base";
+import {Card, CardHeader, Col, Container, Form, FormGroup, Input, Row,Button, CardBody} from "reactstrap";
+import { useState } from "react";
+import {toast } from 'react-toastify';
+import { loginUser } from "../services/user-service";
+import 'react-toastify/dist/ReactToastify.css';
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+ import card from "./card.jpg";
+
+
+function SubmittedForm() {
+
+    return (
+
+
+        <div style={{
+        
+        backgroundImage:'url("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbSRXM2q7wqOzSjoTP03dcb-pk-J1BvB1tWA&usqp=CAU")',width:1277,height:750
+        
+        }}>
+        </div>
+
+
+
+
+
+          );
+        }
+
+        export default SubmittedForm;
+        
+        
+        
